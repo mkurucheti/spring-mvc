329b96708eb37dd27a00f08cd3d2d398b2243003
Murali <muralikrishna.kurucheti@gmail.com>
2017-05-02 11:57:12 +0530

    Initial commit for Spring MVC sample code

---
+++ src/main/java/com/poc/login/LoginController.java
@@ -0,0 +1,17 @@
+package com.poc.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	@ResponseBody
+	public String getLogin(){
+		return "login";
+	}
+
+}
